dc15ba9ea9347e16fcb72a254daff4b43ed9801d
Bryan Avila <bryanjimavila@gmail.com>
2022-12-30 11:43:51 +0800

    add event context and update event hook

--- client/hooks/useEvents.ts
+++ client/hooks/useEvents.ts
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { authContext } from "../store/AuthContext";
+import { useAuth } from "./useAuth";
 import { useFetch } from "./useFetch"
 
 export type EventsDataBody = {
@@ -10,15 +12,29 @@ export type EventsDataBody = {
     ownerName: string
 }
 
+export type EventsDataBodyInput = {
+    title: string,
+    description: string,
+}
+
+export interface useEventsBody {
+    fetchEvents: (data: EventsDataBodyInput) => Promise<void>;
+    createEvent: (data: EventsDataBodyInput) => Promise<void>;
+}
+
 export const useEvents = () => {
     const {fetchUrl} = useFetch();
+    const user = useContext(authContext)
     const [eventsData, setEventsData] = useState<EventsDataBody[]>([])
     const [isLoading, setIsLoading] = useState(true)
 
-    const fetchEvents = async () => {
+    const fetchEvents = async (data: EventsDataBodyInput) => {
         const res = await fetchUrl({
             paths: "events",
-            method: "GET",
+            method: "POST",
+            fetchOptions: {
+                body: JSON.stringify(data)
+            }
         })
         
         if(!res){
@@ -35,5 +51,34 @@ export const useEvents = () => {
         }
     }
 
-    return {}
+    const createEvent = async (data: EventsDataBodyInput) => {
+        const res = await fetchUrl({
+            paths: "events",
+            method: "POST",
+            headerOptions: {
+                Authorization: `Bearer ${user?.userData.accessToken}`
+            },
+            fetchOptions: {
+                body: JSON.stringify(data)
+            }
+        })
+        
+        if(!res){
+            console.log('error')
+        } else {
+            if(res.ok){
+                const data = await res.json();
+                setEventsData(data)
+                setIsLoading(false)
+            } else {
+                console.log("error")
+                setIsLoading(false)
+            }
+        }
+    }
+
+    return {
+        fetchEvents,
+        createEvent
+    }
 }
