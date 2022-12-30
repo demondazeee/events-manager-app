4bc25e4dcca8d780257360567eaf9cd08282fa80
Bryan Avila <bryanjimavila@gmail.com>
2022-12-30 11:40:27 +0800

    add page container layout

---
+++ client/components/layouts/PageContainer.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+import { ContainerLayout } from "./Container"
+
+export const PageContainer = styled(ContainerLayout)`
+    max-width: 600px;
+    padding: 50px 0;
+
+`
