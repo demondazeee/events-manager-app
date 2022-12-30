4963339570ef9148676281b7c5898c550cadfb13
Bryan Avila <bryanjimavila@gmail.com>
2022-12-30 11:38:33 +0800

    update button styles

--- client/components/elements/Buttons.tsx
+++ client/components/elements/Buttons.tsx
@@ -6,17 +6,28 @@ import styles from './link.module.css'
 export const PrimaryButton = styled.button`
     display: block;
     font-size: 2rem;
+    font-weight: 500;
     border: none;
-    background-color: #F43F5E;
+    background-color: #E11845;
     color: #fff;
     padding: 1rem 2rem;
     width: 100%;
-    border-radius: 5px;
-    box-shadow: 0 1rem 2rem rgba(0,0,0,0.25);
+    border-radius: 3px;
+    border: 2.7px solid #000;
+    border-radius: 3px;
+    transition: box-shadow .2s ease-out,
+    transform  .2s ease-out;
 
     &:hover {
         cursor: pointer;
-        background-color: #FB7185;
+        transform: translate(-5px, -5px);
+        box-shadow: 5px 5px #000;
+    }
+
+    &:active {
+        cursor: pointer;
+        transform: translate(-3px, -3px);
+        box-shadow: 3px 3px #000;
     }
 `
 
@@ -25,15 +36,25 @@ export const LinkButton = styled.a`
     display: block;
     font-size: 2rem;
     border: none;
-    background-color: #F43F5E;
+    background-color: #E11845;
     color: #fff;
     padding: 1rem 2rem;
     width: 100%;
-    border-radius: 50px;
+    border: 2px solid #000;
+    border-radius: 3px;
+    transition: box-shadow .2s ease-out,
+    transform  .2s ease-out;
 
     &:hover {
         cursor: pointer;
-        background-color: #BE123C;
+        transform: translate(-5px, -5px);
+        box-shadow: 5px 5px #000;
+    }
+
+    &:active {
+        cursor: pointer;
+        transform: translate(-3px, -3px);
+        box-shadow: 3px 3px #000;
     }
 `
 
