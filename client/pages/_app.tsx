f858854448dd8f42ee60d61b11b4c6d15731dfc6
Bryan Avila <bryanjimavila@gmail.com>
2022-12-30 11:45:19 +0800

    add event context

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,13 +2,16 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Container from '../components/layouts/Container'
 import AuthContext from '../store/AuthContext'
+import EventContext from '../store/EventContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthContext>
-      <Container>
-        <Component {...pageProps} />
-      </Container>
+      <EventContext>
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </EventContext>
     </AuthContext>
   )
 }
