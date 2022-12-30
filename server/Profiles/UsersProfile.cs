e539a0d5438e85001f47ce1f23c5fbe939290983
Bryan Avila <bryanjimavila@gmail.com>
2022-12-30 11:36:11 +0800

    update profiles

---
+++ server/Profiles/UsersProfile.cs
@@ -0,0 +1,25 @@
+using AutoMapper;
+using WebAPI.Entities;
+using WebAPI.Models;
+
+public class UsersProfile: Profile
+{
+    public UsersProfile()
+    {
+        CreateMap<Users, UserDetailDto>();
+        CreateMap<Users, UserDetailWithEventsDto>()
+         .ForMember(dest => dest.Events, opt=>opt.MapFrom(src => src.Events.ToList()));
+
+        CreateMap<Users, AdminDto>();
+        CreateMap<CreateAdminDto, Users>();
+        CreateMap<AuthAdminDto, Users>();
+
+        CreateMap<Users, ManagerDto>();
+        CreateMap<CreateManagerDto, Users>();
+        CreateMap<AuthManagerDto, Users>();    
+
+        CreateMap<Users, MemberDto>();
+        CreateMap<CreateMemberDto, Users>();
+        CreateMap<AuthMemberDto, Users>();
+    }
+}
