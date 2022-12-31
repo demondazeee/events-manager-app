342f1befb9a194773f8128e9db7a57099e86b8f2
Bryan Avila <bryanjimavila@gmail.com>
2022-12-31 13:40:34 +0800

    add event details page

--- client/components/pages/Events/EventDetails.tsx
+++ client/components/pages/Events/EventDetails.tsx
@@ -1,7 +1,68 @@
-const EventDetails = () => {
+import Link from "next/link"
+import styled from "styled-components"
+import {EventsDataBody} from "../../../hooks/useEvents"
+import {H2, H3} from "../../elements/Typography"
+import { Card } from "../../layouts/Card"
+
+type EventItemProps = {
+    data: EventsDataBody
+}
+
+
+const EventContainer = styled.div `
+    padding: 2rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+`
+
+const ImageContainer = styled.div `
+    height: 450px;
+`
+
+const HeaderImage = styled.img `
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: bottom;
+    overflow: hidden;
+`
+
+const EventItemFooter = styled.footer `
+    display: flex;
+    justify-content: space-between;
+`
+
+
+const EventDetails = ({data} : EventItemProps) => {
+    const {id, title, description, createdAt, ownerName} = data;
+    const date = new Date(createdAt)
     return (
         <>
-        
+            <Card>
+                <EventContainer>
+                    <ImageContainer>
+                        <HeaderImage src="/sample.jpg"/>
+                    </ImageContainer>
+                    <H2>{title}</H2>
+                    
+                    <EventItemFooter>
+                        <p>{
+                            date.toDateString()
+                        }</p>
+                        <p>Posted By:
+                            <Link href={
+                                `/profile/${ownerName}`
+                            }>
+                                {ownerName}</Link>
+                        </p>
+                    </EventItemFooter>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, deserunt non at repellat in laudantium quam deleniti, debitis autem omnis rem perferendis eius, soluta asperiores rerum. Molestias laudantium mollitia vero.
+                    </p>
+                </EventContainer>
+            </Card>
         </>
     )
 }
--- client/pages/events/[eventId].tsx
+++ client/pages/events/[eventId].tsx
@@ -1,8 +1,48 @@
-const EventDetailsPage = () => {
+import { GetServerSideProps } from "next"
+import { H3 } from "../../components/elements/Typography"
+import EventDetails from "../../components/pages/Events/EventDetails"
+import { EventsDataBody } from "../../hooks/useEvents"
+
+type EventDetailsPageProp = {
+    data: EventsDataBody
+}
+
+
+const EventDetailsPage = ({data}: EventDetailsPageProp) => {
+    
+    if(!data){
+        return <H3>Loading....</H3>
+    }
+    
     return (
         <>
+            <EventDetails data={data} />
         </>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const {params} = context
+
+    if(!params){
+        return {notFound: true}
+    }
+
+    const url = process.env.NEXT_PUBLIC_SERVER
+    const res = await fetch(`${url}/events/${params.eventId}`)
+
+    if(!res.ok) {
+        return {notFound: true}
+    }
+
+
+    const data = await res.json();
+
+        return {
+        props: {
+            data
+        }
+    }
 }
 
 export default EventDetailsPage
