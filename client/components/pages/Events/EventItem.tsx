be164e15cc2fb6cc5eb4a9dfac15014e523f24ab
Bryan Avila <bryanjimavila@gmail.com>
2022-12-31 13:39:53 +0800

    update event item

--- client/components/pages/Events/EventItem.tsx
+++ client/components/pages/Events/EventItem.tsx
@@ -1,18 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styled from "styled-components";
-import { EventsDataBody } from "../../../hooks/useEvents";
-import { LI } from "../../elements/Lists";
-import { H3 } from "../../elements/Typography";
-import { Card } from "../../layouts/Card";
+import {EventsDataBody} from "../../../hooks/useEvents";
+import {NextLink} from "../../elements/Buttons";
+import {LI} from "../../elements/Lists";
+import {H3} from "../../elements/Typography";
+import {Card} from "../../layouts/Card";
 
 type EventItemProps = {
     data: EventsDataBody
 }
 
+const EventCardContainer = styled(Card)`
+    box-shadow: 0 0 0 0;
+    border: 2px solid #000;
+    border-radius: 5px;
+    transition: box-shadow .2s ease-out, 
+    transform .2s ease-out;
 
+    &:hover {
+        cursor: pointer;
+        box-shadow: 6px 6px #000;
+        transform: translate(-6px, -6px);
+    }
 
-const EventContainer = styled.div`
+`
+
+const EventContainer = styled.div `
     padding: 2rem;
 
     display: flex;
@@ -20,40 +35,51 @@ const EventContainer = styled.div`
     gap: 2rem;
 `
 
-const ImageContainer = styled.div`
+const ImageContainer = styled.div `
 
 `
 
-const HeaderImage = styled.img`
+const HeaderImage = styled.img `
     width: 100%;
     height: 100%;
     object-fit: cover;
     overflow: hidden;
 `
 
-const EventItemFooter = styled.footer`
+const EventItemFooter = styled.footer `
     display: flex;
     justify-content: space-between;
 `
 
-const EventItem = ({data}: EventItemProps) => {
+const EventItem = ({data} : EventItemProps) => {
     const {id, title, createdAt, ownerName} = data;
     const date = new Date(createdAt)
+    const {push} = useRouter()
+
     return (
         <>
-            <LI>
-                <Card>
+            <LI onClick={() =>{
+                push(`/events/${id}`)
+            }}>
+                <EventCardContainer>
                     <EventContainer>
                         <H3>{title}</H3>
                         <ImageContainer>
-                            <HeaderImage src="/sample.jpg" />
+                            <HeaderImage src="/sample.jpg"/>
                         </ImageContainer>
                         <EventItemFooter>
-                            <p>{date.toDateString()}</p>
-                            <p>Posted By: <Link href={`/profile/${ownerName}`}>{ownerName}</Link></p>
+                            <p>{
+                                date.toDateString()
+                            }</p>
+                            <p>Posted By:
+                                <Link href={
+                                    `/profile/${ownerName}`
+                                }>
+                                    {ownerName}</Link>
+                            </p>
                         </EventItemFooter>
                     </EventContainer>
-                </Card>
+                </EventCardContainer>
             </LI>
         </>
     )
