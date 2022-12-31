1f9f3c4df1a6baf395472a1c4ba33bf7efb067c7
Bryan Avila <bryanjimavila@gmail.com>
2022-12-31 13:41:13 +0800

    add event lists on event index page

--- client/pages/events/index.tsx
+++ client/pages/events/index.tsx
@@ -1,8 +1,45 @@
-const Events = () => {
+import { GetServerSideProps } from "next";
+import PageContainer from "../../components/layouts/PageContainer";
+import EventList from "../../components/pages/Events/EventList";
+import { EventsDataBody } from "../../hooks/useEvents";
+
+type EventsPageProp = {
+    data: EventsDataBody[]
+}
+
+
+const Events = ({data}: EventsPageProp) => {
     return (
         <>
+            <PageContainer mainColumn={
+                <EventList eventData={data} />
+            } />
         </>
     )
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+
+    const url = process.env.NEXT_PUBLIC_SERVER
+    const res = await fetch(`${url}/events`)
+
+    if(res.ok) {
+        const data = await res.json();
+
+        return {
+        props: {
+            data
+        }
+        }
+    }
+
+
+    return {
+        props: {
+        data: []
+        }
+    }
+}
+
 
 export default Events;
