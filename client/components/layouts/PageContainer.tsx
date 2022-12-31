f9d3b454a87db755ad30c5073e9101bfddd706f1
Bryan Avila <bryanjimavila@gmail.com>
2022-12-31 13:38:55 +0800

    add new page container component

--- client/components/layouts/PageContainer.tsx
+++ client/components/layouts/PageContainer.tsx
@@ -1,8 +1,55 @@
+import { ReactNode } from "react"
 import styled from "styled-components"
-import { ContainerLayout } from "./Container"
 
-export const PageContainer = styled(ContainerLayout)`
-    max-width: 600px;
+
+
+const PageItemsContainer = styled.div `
     padding: 50px 0;
+`
+
+const PageItemListContainer = styled.div `
+    display: flex;
+
+    gap: 3rem;
+`
+
+const PageItem = styled.div `
+    flex: 1;
+   & > *:not(:last-child) {
+    margin-bottom: 20px;
+   }
+`
+const MainContainer = styled.div `
+    flex: 2;
 
 `
+
+type PageContainerProp = {
+    firstColumn?: ReactNode,
+    mainColumn: ReactNode,
+    lastColumn?: ReactNode
+}
+
+const PageContainer = ({firstColumn, mainColumn, lastColumn}: PageContainerProp) => {
+    return (
+        <>
+
+            <PageItemsContainer>
+                <PageItemListContainer>
+                    <PageItem>
+                        {firstColumn}
+                    </PageItem>
+                    <MainContainer> 
+                        {mainColumn}
+                    </MainContainer>
+                    <PageItem>
+                        {lastColumn}
+                    </PageItem>
+                </PageItemListContainer>
+            </PageItemsContainer>
+        </>
+    )
+}
+
+
+export default PageContainer
