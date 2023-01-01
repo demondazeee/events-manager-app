b98a91f62451ecfffec2ac8a367ad6bfd8c79f86
Bryan Avila <bryanjimavila@gmail.com>
2023-01-01 19:42:14 +0800

    add category context and hook

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,14 +3,17 @@ import type { AppProps } from 'next/app'
 import Container from '../components/layouts/Container'
 import AuthContext from '../store/AuthContext'
 import EventContext from '../store/EventContext'
+import CategoryContext from '../store/CategoryContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthContext>
       <EventContext>
-        <Container>
-          <Component {...pageProps} />
-        </Container>
+        <CategoryContext>
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+        </CategoryContext>
       </EventContext>
     </AuthContext>
   )
