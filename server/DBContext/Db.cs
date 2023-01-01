ae1ec4b79fe527d05c08b2a6a9b22e5d2a9be1c9
Bryan Avila <bryanjimavila@gmail.com>
2023-01-01 19:41:04 +0800

    add category as dbset

--- server/DBContext/Db.cs
+++ server/DBContext/Db.cs
@@ -10,6 +10,9 @@ public class Db : DbContext
 {
     public DbSet<Users> Users => Set<Users>();
     public DbSet<Events> Events => Set<Events>();
+
+    public DbSet<Category> Categories => Set<Category>();
+
     private readonly IConfiguration config; 
     public Db(
         DbContextOptions opt,
@@ -40,6 +43,21 @@ public class Db : DbContext
                 Password = Argon2.Hash(config),
                 Email = "admin@admin.com",
                 Role = Entities.Users.UserRole.Admin
+            }
+        );
+
+        modelBuilder.Entity<Category>().HasData(
+            new {
+                Id = new Guid("deee9a3d-b385-4c98-b452-897003a0b497"),
+                Name = "Music",
+            },
+            new {
+                Id = new Guid("8e3eb515-b114-4a3f-98e6-ce37bbb1386e"),
+                Name = "Visual Arts and Performance",
+            },
+            new {
+                Id = new Guid("52e56a6b-52bc-4f91-bac4-2406f87cc94d"),
+                Name = "Business",
             }
         );
         base.OnModelCreating(modelBuilder);
