afd2f1318e61bdb06ae3a8877da8ba8bdd94bf0d
Bryan Avila <bryanjimavila@gmail.com>
2023-01-02 20:46:14 +0800

    add manager login page

---
+++ client/pages/manager/login.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const TestContainer = styled.div`
+    height: 100vh;
+`
+
+const ManagerLoginPage = () => {
+    
+    return (
+        <>
+            <p>hello</p>
+        </>
+    )
+}
+
+export default ManagerLoginPage
