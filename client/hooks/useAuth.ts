7e18edf260b20bf0dc82d192484c49675d749d8f
Bryan Avila <bryanjimavila@gmail.com>
2023-01-02 20:45:05 +0800

    add error handler in login

--- client/hooks/useAuth.ts
+++ client/hooks/useAuth.ts
@@ -1,10 +1,14 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { EventsDataBody } from "./useEvents"
 import { useFetch } from "./useFetch"
+
+import {toast} from 'react-toastify'
+import { toastContext } from "../store/ToastContext"
 
 export interface UserDataBody  {
     id: string
     username: string
+    role: number;
     accessToken: string
 }
 
@@ -17,24 +21,33 @@ export type LoginMemberBody = {
     password: string
 }
 
+type LoginUserProp = {
+    path: string,
+    userLoginInput: LoginMemberBody
+}
+
 export interface useAuthBody {
     isLoading: boolean;
     isLoggedIn: boolean;
     userData: UserDataBody;
-    loginUser: ({ username, password }: LoginMemberBody) => Promise<void>;
+    loginUser:  ({ path, userLoginInput: { username, password } }: LoginUserProp) => Promise<void>
     refreshToken:() => Promise<void>;
     logoutUser: () => Promise<void>;
 }
 
 export const useAuth = () => {
-    const [userData, setUserData] = useState<UserDataBody>({id: "", username: "", accessToken: ""})
+    const [userData, setUserData] = useState<UserDataBody>({id: "", username: "", role: 0, accessToken: ""})
     const [isLoading, setIsLoading] = useState(false)
     const [isLoggedIn, setIsLoggedIn] = useState(true)
     const {fetchUrl} = useFetch()
-    const loginUser = async ({username, password}: LoginMemberBody) => {
+    const {toastMessage} = useContext(toastContext)
+
+
+
+    const loginUser = async ({path, userLoginInput: {username, password}}: LoginUserProp) => {
         setIsLoading(true)
         const res = await fetchUrl({
-            paths: "user/member/login",
+            paths: `user/${path}/login`,
             method: "POST",
             fetchOptions: {
                 body: JSON.stringify({
@@ -45,15 +58,23 @@ export const useAuth = () => {
         })
 
         if(res == undefined){
-            console.log('error')
+            console.log('zzzz')
+
         } else {
             if(res.ok){
                 const data = await res.json();
                 setUserData(data)
                 setIsLoading(false)
                 setIsLoggedIn(true)
+                toastMessage({
+                    message: `Log In successfully! Welcome ${data.username}!`,
+                    type: "success"
+                })
             } else {
-                console.log("error")
+                toastMessage({
+                    message: "Invalid Log in",
+                    type: "error"
+                })
                 setIsLoading(false)
                 
             }
@@ -93,7 +114,7 @@ export const useAuth = () => {
             console.log('error')
         } else {
             if(res.ok){
-                setUserData({id: "", username: "", accessToken: ""})
+                setUserData({id: "", username: "", role: 0, accessToken: ""})
                 setIsLoading(false)
                 setIsLoggedIn(false)
             } else {
