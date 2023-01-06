cab8af5bf167510b7b2aabce51fd3894a5a28ae8
Bryan Avila <bryanjimavila@gmail.com>
2023-01-07 06:10:05 +0800

    add update event dto

---
+++ server/Models/Events/UpdateEventsDto.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using WebAPI.Entities;
+
+public class UpdateEventsDto
+{
+    
+    public string? HeaderImage { get; set; }
+
+    [Required]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    public string Description { get; set; } = string.Empty;
+
+    // Duration of the event
+    [Required]
+    public DateTime FromDate { get; set; }
+
+    [Required]
+    public DateTime ToDate { get; set; }
+
+    [Required]
+    public string Location { get; set; } = string.Empty;
+
+    [Required]
+    public string Category {get; set; } = string.Empty;
+    
+    [Required]
+    public bool IsFree {get; set;}
+
+    public bool IsClosed {get; set;}
+
+}
