cfaa7109eefd868cfc853cad47b30ae9cd187daf
Bryan Avila <bryanjimavila@gmail.com>
2023-01-17 20:23:10 +0800

    update app wrappers

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,20 +2,28 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import AuthContext from '../store/AuthContext'
 import EventContext from '../store/EventContext'
-import { ToastContainer } from 'react-toastify';
+// import { ToastContainer } from 'react-toastify';
+import {Toaster} from 'react-hot-toast'
 import 'react-toastify/dist/ReactToastify.css';
 import ToastContext from '../store/ToastContext'
+import {QueryClient, QueryClientProvider} from 'react-query'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient()
+
   return (
    <>
-   <ToastContainer />
+   <Toaster />
     <ToastContext>
-      <AuthContext>
-        <EventContext>
-          <Component {...pageProps} />
-        </EventContext>
-      </AuthContext>
+      <QueryClientProvider client={
+        queryClient
+      }>
+        <AuthContext>
+          <EventContext>
+            <Component {...pageProps} />
+          </EventContext>
+        </AuthContext>
+      </QueryClientProvider>
     </ToastContext>
    </>
   )
